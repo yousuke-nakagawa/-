5c728831b5e8f4dd0cdd122aa9c16cc25890397e
yousuke-nakagawa <sunshine.energy1982@gmail.com>
2021-01-21 13:44:46 +0000

    finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # ページごとにタイトルを返す  
+  def full_title(page_name = "") # メソッドの引数の定義
+    base_title = "AttendanceApp" # 基本となるアプリケーション名を変数に代入
+    if page_name.empty? # 引数を受け取っているか判定
+      base_title # 
+    else # 引数page_nameが空文字の場合はbase_titleのみ返す
+      page_name + " | " + base_title #
+    end
+  end
 end
